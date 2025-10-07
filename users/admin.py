bb22daa92055ee735d73ba48eb92ddb77ca02e62
daniel4191 <daniell8282@naver.com>
2025-10-07 17:06:57 +0900

    admin의 User에 추가필드 생성

---
+++ users/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+
+from .models import User
+
+group_name = ""
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets =[
+        (None, {"fields": ("username", "password")}),
+        ("개인정보", {"fields": ("first_name", "last_name", "email")}),
+        ("추가필드", {"fields": ("profile_image", "short_description")}),
+        ("권한", {"fields": ("is_staff", "is_active", "is_superuser",)}),
+        
+        ("중요한 일정", {"fields": ("last_login", "date_joined")}),
+        
+    ]
+    
